d7af61d3455fc0547789c41d753270d26d676d54
vanjasoa <vanjasoa@example.com>
2023-11-14 06:40:15 +0300

    fix: :bug: auth in directus

--- components/QrCodeContent.vue
+++ components/QrCodeContent.vue
@@ -1,7 +1,8 @@
 <template>
         <div class="flex flex-col justify-center items-center">
             <slot name="retour" />
-            <!--div class="my-16">
+            <!--
+                div class="my-16">
                 <QRCodeVue3 value="Simple QR code" />
             </div-->
             <h1 class="text-xl font-extrabold">Présentez votre pass</h1>
